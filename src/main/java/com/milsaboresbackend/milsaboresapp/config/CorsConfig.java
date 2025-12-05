7f46e72f424241d7b8ac9dbf90d1708910b7e2a0
Alvaro Chavez <a.chavezs@duocuc.cl>
2025-12-04 23:49:03 -0300

    CONFIG: CAMBIO EN CORS

--- src/main/java/com/milsaboresbackend/milsaboresapp/config/CorsConfig.java
+++ src/main/java/com/milsaboresbackend/milsaboresapp/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://milsabores-app.s3-website-us-east-1.amazonaws.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
